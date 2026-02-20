71ab523235e16a2979f3ad02aa923149e4ca9b45
UC-Burdwan <unitedcomputerburdwan2133@gmail.com>
2026-02-20 19:38:27 +0530

    soumyadeb ads

---
+++ pd.new.py
@@ -0,0 +1,9 @@
+import pandas as pd
+data = pd.DataFrame({
+    "Employee" : ['Raj','Neha','Raju','Arjun'],
+    "Working hours" : [8,7,9,6],
+    "Project Name" : ["AI Model","Data Analysis", "Web Dev", "Cloud"],
+
+    })
+print(data)
+    
